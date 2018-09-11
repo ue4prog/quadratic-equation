1758dcd89218b7ec139c2981e9f13f67e15e8d94
ue4prog <vladtet2000@mail.ru>
2018-09-11 22:10:09 +0300

    your tests semells like sad spirit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  equation = equation.split(' ');
+  equation[3] += equation[4];
+  equation[4] = 'Fuck, GG';
+  equation[7] += equation[8];
+  equation[8] = 'Fuck, GG';
+  equation = equation.map(el => parseInt(el)).filter(el => !isNaN(el))
+  const desc = equation[1] **2 - 4 * equation[0] * equation[2];
+  const result = [
+    Math.round((-equation[1] + Math.sqrt(desc) ) / (2 * equation[0])),
+    Math.round((-equation[1] - Math.sqrt(desc) ) / (2 * equation[0]))
+  ];
+  return result.sort((a ,b) => a - b);
 }
